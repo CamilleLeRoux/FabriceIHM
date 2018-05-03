0690258171718fe7c7276167191c2156a68828e3
Camille Le Roux <camille.le-roux1@etu.unice.fr>
2018-05-03 19:00:45 +0200

    Map in declaration 1

--- app/src/main/java/fr/unice/polytech/dipn/IncidentForm.java
+++ app/src/main/java/fr/unice/polytech/dipn/IncidentForm.java
@@ -1,7 +1,13 @@
 package fr.unice.polytech.dipn;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,24 +15,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.logging.Logger;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import fr.unice.polytech.dipn.DataBase.AppDatabase;
+
 import fr.unice.polytech.dipn.DataBase.Incident;
 import fr.unice.polytech.dipn.DataBase.IncidentViewModel;
 
-import static fr.unice.polytech.dipn.IncidentPreviewFragment.NEW_WORD_ACTIVITY_REQUEST_CODE;
 
-public class IncidentForm extends AppCompatActivity {
+public class IncidentForm extends AppCompatActivity implements OnMapReadyCallback {
+
+
 
     public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
     private IncidentViewModel incidentViewModel;
+    private GoogleMap googleMap;
+    private Location location;
+    private LocationManager mLocationManager;
+
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState){
 
         incidentViewModel = ViewModelProviders.of(this).get(IncidentViewModel.class);
 
@@ -37,29 +56,14 @@ public class IncidentForm extends AppCompatActivity {
 
         final EditText editAuthor = findViewById((R.id.editAuthor));
         final EditText editTitle = findViewById((R.id.editTitle));
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-
-
-
-        /*save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Date currentTime = Calendar.getInstance().getTime();
-                SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-                String formattedDate = df.format(currentTime);
-                String author = editAuthor.getText().toString();
-                String title = editTitle.getText().toString();
-                if (!author.equals("") && !title.equals("")) {
-                    Data.addIncident(title,author,1,1,2,1,"YOLO",formattedDate);
-                    Intent intent = new Intent(IncidentForm.this, IncidentList.class);
-                    startActivity(intent);
-                }
-            }
-        });*/
 
         save.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent replyIntent = new Intent();
+                public void onClick(View view) {
+                    Intent replyIntent = new Intent();
                 if ((TextUtils.isEmpty(editAuthor.getText()) && TextUtils.isEmpty(editTitle.getText())) || TextUtils.isEmpty(editAuthor.getText()) || TextUtils.isEmpty(editTitle.getText())) {
                     setResult(RESULT_CANCELED, replyIntent);
                 } else {
@@ -73,7 +77,51 @@ public class IncidentForm extends AppCompatActivity {
                     startActivity(intent);
                 }
                 finish();
-            }
+                }
         });
     }
+
+
+    //Getting current location
+    private void getCurrentLocation() {
+        //googleMap.clear();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+        }
+
+        /*boolean isGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (isNetworkEnabled) {
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, mLocationListener);
+            location = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+        if (isGPSEnabled) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    LOCATION_UPDATE_MIN_TIME, LOCATION_UPDATE_MIN_DISTANCE, mLocationListener);
+            location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }*/
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        //getCurrentLocation();
+        LatLng position = new LatLng(43.615788,7.072512);
+        googleMap.addMarker(new MarkerOptions().position(position)
+                .title("Polytech Batiment E"));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 12));
+    }
+
+
 }
