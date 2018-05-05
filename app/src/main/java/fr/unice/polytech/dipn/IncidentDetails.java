4293eb8008228b36f744d192ee3daf44123883a1
Francois Dore <francois.dore@etu.unice.fr>
2018-05-05 15:13:50 +0200

    Update de IncidentDetails

--- app/src/main/java/fr/unice/polytech/dipn/IncidentDetails.java
+++ app/src/main/java/fr/unice/polytech/dipn/IncidentDetails.java
@@ -2,6 +2,7 @@ package fr.unice.polytech.dipn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import fr.unice.polytech.dipn.DataBase.Incident;
@@ -17,8 +18,27 @@ public class IncidentDetails extends AppCompatActivity {
         this.incident = (Incident) getIntent().getSerializableExtra("Incident");
 
         final TextView title = findViewById(R.id.detailTitle);
+        final TextView author = findViewById(R.id.detailAuthor);
+        final TextView date = findViewById(R.id.detailDate);
+        final TextView description = findViewById(R.id.detailDescription);
+        final ImageView icon = findViewById(R.id.detailIcon);
 
         title.setText(incident.getTitle());
+        author.setText(incident.getAuthor());
+        date.setText(incident.getDate());
+        description.setText(incident.getDescription());
+
+        switch(incident.getImportance()) {
+            case 1:
+                icon.setImageResource(R.drawable.emergency1);
+                break;
+            case 2:
+                icon.setImageResource(R.drawable.emergency2);
+                break;
+            case 3:
+                icon.setImageResource(R.drawable.emergency3);
+                break;
+        }
 
     }
 }
