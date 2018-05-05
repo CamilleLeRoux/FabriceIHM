d13e4cf9910159adc56dd57b171f7f9ae8666744
Camille Le Roux <camille.le-roux1@etu.unice.fr>
2018-05-05 16:25:51 +0200

    Incident form popup for user's position 1

--- app/src/main/java/fr/unice/polytech/dipn/IncidentForm.java
+++ app/src/main/java/fr/unice/polytech/dipn/IncidentForm.java
@@ -2,12 +2,14 @@ package fr.unice.polytech.dipn;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -46,6 +48,24 @@ public class IncidentForm extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     protected void onCreate (Bundle savedInstanceState){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(IncidentForm.this,R.style.MyAlertDialogStyle);
+
+        builder.setMessage(R.string.popup_message)
+                .setTitle(R.string.popup_title);
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+
+        builder.show();
 
         incidentViewModel = ViewModelProviders.of(this).get(IncidentViewModel.class);
 
