4b640361a175e403759d0804ef3f6d037454af47
Francois Dore <francois.dore@etu.unice.fr>
2018-05-14 16:11:21 +0200

    Updates mineures

--- app/src/main/java/fr/unice/polytech/dipn/MainActivity.java
+++ app/src/main/java/fr/unice/polytech/dipn/MainActivity.java
@@ -2,8 +2,6 @@ package fr.unice.polytech.dipn;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -23,9 +21,18 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Button adminButton = (Button) findViewById(R.id.adminButton);
+        Button technicButton = (Button) findViewById(R.id.technicButton);
         Button userButton = (Button) findViewById(R.id.userButton);
 
         adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, IncidentList.class);
+                startActivity(intent);
+            }
+        });
+
+        technicButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, IncidentList.class);
