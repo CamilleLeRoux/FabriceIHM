abd3c5b16dd24af6fd8679f3a518d73cbf155a66
Francois Dore <francois.dore@etu.unice.fr>
2018-05-24 21:53:19 +0200

    Bricolage Twitter

--- app/src/main/java/fr/unice/polytech/dipn/TwitterList.java
+++ app/src/main/java/fr/unice/polytech/dipn/TwitterList.java
@@ -1,10 +1,12 @@
 package fr.unice.polytech.dipn;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 
 import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Twitter;
@@ -14,15 +16,21 @@ import com.twitter.sdk.android.core.TwitterConfig;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.tweetui.SearchTimeline;
+import com.twitter.sdk.android.tweetui.UserTimeline;
 
 public class TwitterList extends AppCompatActivity {
 
+    boolean isHashTag;
+    private RecyclerView rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.isHashTag=true;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter_list);
 
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.tweetList);
+        this.rv = recyclerView;
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -35,16 +43,38 @@ public class TwitterList extends AppCompatActivity {
         final TwitterSession session = new TwitterSession(new TwitterAuthToken(getString(R.string.com_twitter_sdk_android_ACCESS_KEY), getString(R.string.com_twitter_sdk_android_ACCESS_SECRET)), 985877416857034752L, "pbunice");
         TwitterCore.getInstance().getSessionManager().setActiveSession(session);
 
-        final SearchTimeline searchTimeline = new SearchTimeline.Builder()
-                .query("#Polytech")
-                .maxItemsPerRequest(50)
-                .build();
+        switchTimeLine();
+        FloatingActionButton twitSwitch = findViewById(R.id.twitterSwitch);
+        twitSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchTimeLine();
+            }
+        });
+    }
 
-        final CustomTweetTimeLineAdapter adapter = new CustomTweetTimeLineAdapter(this,
-                searchTimeline,
-                R.style.tw__TweetLightWithActionsStyle,
-                null);
+    public void switchTimeLine() {
+        final CustomTweetTimeLineAdapter adapter;
+        if (isHashTag) {
+            final SearchTimeline searchTimeline = new SearchTimeline.Builder()
+                    .query("#Polytech")
+                    .maxItemsPerRequest(50)
+                    .build();
 
-        recyclerView.setAdapter(adapter);
+            adapter = new CustomTweetTimeLineAdapter(this,
+                    searchTimeline,
+                    R.style.tw__TweetLightWithActionsStyle,
+                    null);
+        } else {
+            UserTimeline userTimeline = new UserTimeline.Builder().screenName("pbunice").build();
+
+            adapter = new CustomTweetTimeLineAdapter(this,
+                    userTimeline,
+                    R.style.tw__TweetLightWithActionsStyle,
+                    null);
+        }
+        rv.setAdapter(adapter);
+        this.isHashTag=!this.isHashTag;
+
     }
 }
